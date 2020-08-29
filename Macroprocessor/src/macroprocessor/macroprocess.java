bd67e61528bb554550e5b2bf3e67fe9520ca70b9
ShivamShrirao <shivamshrirao@gmail.com>
2020-08-29 22:39:33 +0530

    commented

--- Macroprocessor/src/macroprocessor/macroprocess.java
+++ Macroprocessor/src/macroprocessor/macroprocess.java
@@ -25,68 +25,81 @@ public class macroprocess {
         LinkedHashMap<String, ArrayList<String>> arg_names = new LinkedHashMap<>(); // macro name, list of arguments
         LinkedHashMap<String, String> ALA = new LinkedHashMap<>();                  // arg name, value
 
-        int line_num=0;
         boolean in_macro=false, name_in_next=false;
         String currentLine;
-        // read till EOF
+        // read till EOF, Pass 1
         while ((currentLine = codeReader.readLine()) != null) {
-            line_num++;
             if (currentLine.startsWith("#"))       // '#' is a comment
                 continue;
             ArrayList<String> tokens = to_tokens(currentLine);
-            if (tokens.size() > 0) {
-                boolean check_macro = true;
-                if (in_macro) {
+            if (tokens.size() > 0) {                    // if line not empty
+                if (in_macro) {                         // process inside macro
                     if (tokens.get(0).equals("MEND")) {
-                        in_macro = false;
+                        in_macro = false;               // mark end of macro
                     }
-                    else if (name_in_next) {
-                        MNT.put(tokens.get(0), MDT.size()-1);
-                        check_macro = false;
+                    else if (name_in_next) {            // get name of macro from next line
+                        MNT.put(tokens.get(0), MDT.size()-1);       // save name of macro
                         ArrayList<String> al = new ArrayList<>();
-                        for (int i = 1; i<tokens.size(); i++)
+                        for (int i = 1; i<tokens.size(); i++)       // iterate through arguments of macro
                             al.add(tokens.get(i));
                         arg_names.put(tokens.get(0), al);
                         name_in_next = false;
-                        outWriter.write(currentLine+"\n");
                     }
-                    MDT.add(currentLine);
+                    MDT.add(currentLine);               // save macro line to MDT
                 }
-                else if (tokens.get(0).equals("MACRO")) {
-                    if (tokens.size() > 1) {
-                        MNT.put(tokens.get(1), MDT.size());
+                else if (tokens.get(0).equals("MACRO")) {           // check start of macro
+                    if (tokens.size() > 1) {                        // if it contains the name of macro too
+                        MNT.put(tokens.get(1), MDT.size());         // save name of macro
                         ArrayList<String> al = new ArrayList<>();
-                        for (int i = 2; i<tokens.size(); i++)
+                        for (int i = 2; i<tokens.size(); i++)       // iterate through arguments of macro
                             al.add(tokens.get(i));
                         arg_names.put(tokens.get(1), al);
                     }
                     else
-                        name_in_next = true;
+                        name_in_next = true;            // set to look for macro name in next line
                     MDT.add(currentLine);
                     in_macro = true;
                 }
-                if(check_macro){
-                    if (MNT.get(tokens.get(0)) != null){
-                        String mname = tokens.get(0);
-                        int i = 1;
-                        for(String argnm: arg_names.get(mname))
-                            ALA.put(argnm, tokens.get(i++));
-                        Integer idx = MNT.get(mname);
-                        while (!MDT.get(idx).contains("MEND")) {
-                            if(!MDT.get(idx).contains("MACRO") && !MDT.get(idx).contains(mname)) {
-                                String line = MDT.get(idx) + "\n";
-                                for(String argnm: arg_names.get(mname))
-                                    line = line.replace(argnm, ALA.get(argnm));
-                                outWriter.write(line);
-                            }
-                            idx++;
+            }
+        }
+        // Pass 2
+        codeReader = new BufferedReader(new FileReader("macro_inp.txt"));
+        in_macro = false;
+        while ((currentLine = codeReader.readLine()) != null) {
+            if (currentLine.startsWith("#"))       // '#' is a comment
+                continue;
+            ArrayList<String> tokens = to_tokens(currentLine);
+            if (tokens.size() > 0) {
+                if (currentLine.contains("MACRO")) {
+                    outWriter.write(currentLine + "\n");        // if macro, just write to file
+                    in_macro = true;                                // mark start of macro
+                }
+                else if (currentLine.contains("MEND")) {
+                    outWriter.write(currentLine + "\n");
+                    in_macro = false;                               // mark end of macro
+                }
+                else if (MNT.get(tokens.get(0)) != null && !in_macro){  // if macro name found and not in a macro definition
+                    String mname = tokens.get(0);
+                    int i = 1;
+                    for(String argnm: arg_names.get(mname))
+                        ALA.put(argnm, tokens.get(i++));            // get values of arguments
+                    Integer idx = MNT.get(mname);                   // get index of macro definition
+                    while (!MDT.get(idx).contains("MEND")) {        // till end of macro
+                        if(!MDT.get(idx).contains("MACRO") && !MDT.get(idx).contains(mname)) {
+                            String line = MDT.get(idx) + "\n";      // get macro line
+                            for(String argnm: arg_names.get(mname))
+                                line = line.replace(argnm, ALA.get(argnm)); // replace the argument with value
+                            outWriter.write(line);
                         }
+                        idx++;
                     }
-                    else
-                        outWriter.write(currentLine+"\n");
+                }
+                else {
+                    outWriter.write(currentLine + "\n");
                 }
             }
         }
+
         System.out.println("MDT-");
         for (int i=0; i < MDT.size(); i++) {
             System.out.println(i+" "+MDT.get(i));
