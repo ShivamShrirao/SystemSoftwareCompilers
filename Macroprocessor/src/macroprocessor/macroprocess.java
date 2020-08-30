8fcd7dee81f29d10416458f1029afe1d2549a991
ShivamShrirao <shivamshrirao@gmail.com>
2020-08-30 21:07:51 +0530

    recursive macro expansion

--- Macroprocessor/src/macroprocessor/macroprocess.java
+++ Macroprocessor/src/macroprocessor/macroprocess.java
@@ -5,6 +5,11 @@ import java.util.*;
 
 public class macroprocess {
 
+    static ArrayList<String> MDT = new ArrayList<>();                                  // macro definition table
+    static LinkedHashMap<String, Integer> MNT = new LinkedHashMap<>();                 // macro name, index in definition
+    static LinkedHashMap<String, ArrayList<String>> arg_names = new LinkedHashMap<>(); // macro name, list of arguments
+    static LinkedHashMap<String, String> ALA = new LinkedHashMap<>();                  // arg name, value
+
     static ArrayList<String> to_tokens(String line) {
         String currentLine = line.replace(',', ' ');       // replace ',' with space to split
         String[] tokens_spl = currentLine.split("\\s+");              // split by one or more space
@@ -16,14 +21,38 @@ public class macroprocess {
         return tokens;
     }
 
+    static void expand_macro(ArrayList<String> tokens, BufferedWriter outWriter, int depth) throws IOException {
+        String mname = tokens.get(0);
+        int i = 1;
+        for(String argnm: arg_names.get(mname))
+            ALA.put(argnm, tokens.get(i++));            // get values of arguments
+        Integer idx = MNT.get(mname);                   // get index of macro definition
+        while (!MDT.get(idx).contains("MEND")) {        // till end of macro
+            if(!MDT.get(idx).contains("MACRO") && !MDT.get(idx).contains(mname)) {      // if not MACRO start and not declaration of name.
+                String line = MDT.get(idx);             // get macro line
+                for(String argnm: arg_names.get(mname)) {
+                    line = line.replace(argnm, ALA.get(argnm)); // replace the argument with value
+                }
+                ArrayList<String> ltoks = to_tokens(line);
+                if (MNT.get(ltoks.get(0)) != null){     // macro found inside a macro
+                    if (depth<5){                       // check depth of recursion
+                        expand_macro(ltoks, outWriter, ++depth);
+                    }
+                    else {
+                        System.out.println("[!] Maximum recursion depth reached. Check macro definition !");
+                        return;
+                    }
+                }
+                else
+                    outWriter.write(line + "\n");
+            }
+            idx++;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader codeReader = new BufferedReader(new FileReader("macro_inp.txt"));
         BufferedWriter outWriter = new BufferedWriter(new FileWriter("output.txt"));
-
-        ArrayList<String> MDT = new ArrayList<>();                                  // macro definition table
-        LinkedHashMap<String, Integer> MNT = new LinkedHashMap<>();                 // macro name, index in definition
-        LinkedHashMap<String, ArrayList<String>> arg_names = new LinkedHashMap<>(); // macro name, list of arguments
-        LinkedHashMap<String, String> ALA = new LinkedHashMap<>();                  // arg name, value
 
         boolean in_macro=false, name_in_next=false;
         String currentLine;
@@ -79,20 +108,7 @@ public class macroprocess {
                     in_macro = false;                               // mark end of macro
                 }
                 else if (MNT.get(tokens.get(0)) != null && !in_macro){  // if macro name found and not in a macro definition
-                    String mname = tokens.get(0);
-                    int i = 1;
-                    for(String argnm: arg_names.get(mname))
-                        ALA.put(argnm, tokens.get(i++));            // get values of arguments
-                    Integer idx = MNT.get(mname);                   // get index of macro definition
-                    while (!MDT.get(idx).contains("MEND")) {        // till end of macro
-                        if(!MDT.get(idx).contains("MACRO") && !MDT.get(idx).contains(mname)) {
-                            String line = MDT.get(idx) + "\n";      // get macro line
-                            for(String argnm: arg_names.get(mname))
-                                line = line.replace(argnm, ALA.get(argnm)); // replace the argument with value
-                            outWriter.write(line);
-                        }
-                        idx++;
-                    }
+                    expand_macro(tokens, outWriter, 0);
                 }
                 else {
                     outWriter.write(currentLine + "\n");
